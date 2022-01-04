1561d5c1116803ae2ecd719aff9d0819fe8ff377
Michi <mic.michael@gmx.de>
2022-01-04 14:29:30 +0100

    does the consent get stored at all?

--- registry-contracts/contracts/ConsentRegistry.sol
+++ registry-contracts/contracts/ConsentRegistry.sol
@@ -11,7 +11,7 @@ contract ConsentRegistry {
     mapping(bytes32 => Consent) hashToConsent;
 
     // Checks whether the given hash was ever stored to the blockchain and was not yet revoked
-    function hashExists(bytes32 consentHash) public view returns(bool){
+    function hashExists(bytes32 consentHash) public view returns(bool) {
         Consent storage consentObj = hashToConsent[consentHash];
         if (consentObj.timestamp != 0 && consentObj.revoked != true) {
             return true;
@@ -33,7 +33,6 @@ contract ConsentRegistry {
     // Sets the revoked attribute of the given hash to false and updates timestamp
     function revokeConsent(bytes32 consentHash) public {
         if (!hashExists(consentHash)) revert("Provided consent hash not found on chain.");
-        hashToConsent[consentHash].revoked = true;
-        hashToConsent[consentHash].timestamp = now;
+        _createConsent(Consent(consentHash, now, false));
     }
 }
